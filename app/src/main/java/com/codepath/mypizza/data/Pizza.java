9761d683c468955232ebdb27010180dce00e6f29
Satria <72090177+wibawasatria@users.noreply.github.com>
2021-04-26 18:56:21 +0800

    Add files via upload

---
+++ app/src/main/java/com/codepath/mypizza/data/Pizza.java
@@ -0,0 +1,19 @@
+package com.codepath.mypizza.data;
+
+/**
+ * Created by Shyam Rokde on 8/5/16.
+ */
+public class Pizza {
+
+  public static String[] pizzaMenu = {
+      "Espresso",
+      "Affogato",
+      "Macchiato"
+  };
+
+  public static String[] pizzaDetails = {
+      "Espresso adalah minuman yang dihasilkan dengan mengekstraksi biji kopi yang sudah digiling dengan menyemburkan air panas di bawah tekanan tinggi.[1] Espresso berasal dari Bahasa Italia yang berarti express atau \"cepat\" karena dibuat untuk disajikan dengan segera kepada pelanggan.",
+      "Affogato adalah sajian es krim dalam cangkir atau mangkuk kecil yang disiram dengan espresso. Sajian kopi unik ini berasal dari Italia. Dalam bahasa Italia affogato berarti tenggelam. Maksud dari penamaan itu adalah untuk menggambarkan tenggelamnya satu scoop es krim di dalam segelas espresso.",
+      "Moccacino adalah minuman kopi yang terbuat dari campuran espresso dengan coklat dan susu. Saat ini campuran Mocha-Java biasa dicampur dengan varietas lainnya untuk menciptakan ciri khas yang unik. Banyak perusahaan kopi yang memiliki campurannya tersendiri. Komposisinya diracik dari bubuk cokelat, kopi Vienna, susu panas & busa. Di Eropa, biasanya dikenal sebagai Moccacino. Nama Mocha sendiri sebenarnya adalah sebuah nama varietas kopi dari daerah Mocha, Yaman."
+  };
+}
